9ca1d1b16f90a99b3cd335813f4952c5cf8920bf
Frixoe <suryansh5060@gmail.com>
2017-12-24 20:53:37 +0900

    Add 'here' functionality and 'get' functinality.
    Added the feature to get the path to the templates directory with 'fot
    get' and to set the templates directory to the current working directory
    with 'fot here'.

--- README.md
+++ README.md
@@ -16,12 +16,15 @@ Open your command line/terminal and type the following:
 **Setup**
 -----
 
-####To setup your installation of fot do the following:
+To let **fot** know where to look for, when cloning templates.
+####To setup your installation of **fot** do the following:
 
-1. Create a folder containing all of the templates you would like to use and remember the path.
-2. Type the following in your cmd/terminal:  `fot set {{your template path}}`
- 
-3. That is it!
+1. Create a folder containing all of the templates you would like to use and navigate into that folder using your cmd/terminal.
+2. Type the following in your cmd/terminal:  `fot here`
+>Alternatively: You could use `fot set {{arg}}`
+>Here - '**arg**' is the absolute path to ***inside*** your templates folder.
+
+3. That's it, You're all set! Mom's gonna be really proud...
 
 >Note: You can change your templates directory by doing the steps above with another path.
 
@@ -31,7 +34,7 @@ Open your command line/terminal and type the following:
 -----
 
 ####To clone a template simply:
-1. Navigate into your project folders parent directory.
+1. Navigate into your project folders ***parent directory***.
 2. Type `fot clone {{template name/folder name(inside your templates folder)}}`
 
 ---
--- cli.js
+++ cli.js
@@ -4,9 +4,18 @@ var shell = require('shelljs');
 
 var setPath = require('./commands/setPath');
 var cloneDir = require('./commands/cloneDir');
+var getPath = require('./commands/getPath');
 
 var helpString = `USAGE: "fot {command} {argument}"
-"Create folder templates with ease."`;
+"Create folder templates with ease."
+
+Commands:
+- 'fot here': Set the templates directory to the working directory.
+- 'fot get': Get the template directory's absolute path.
+- 'fot set {abs-path}': Set the templates directory to the absolute path {abs-path}
+- 'fot clone {template}': Clone a template into the working directory.
+
+With great node packages come ridiculous bugs so please do report them :D`;
 
 var command = args[0];
 var input = args[1];
@@ -21,10 +30,23 @@ switch (command)
             shell.exit(1);
         } else console.error('ERROR: Incorrect option usage! Correct: "fot set {templates path(string)}"');
         break;
+    case 'here':
+        if (!input)
+        {
+            setPath(shell.pwd().stdout);
+            shell.exit(1);
+        } else console.error('ERROR: Incorrect option usage! Correct: "fot here"');
+        break;
+    case 'get':
+        if (!input)
+        {
+            console.log("INFO: The templates directory is set to - " + getPath());
+            shell.exit(1);
+        } else console.error('ERROR: Incorrect option usage! Correct: "fot get"');
     case 'clone':
         if (isString(input))
         {
-            cloneDir(input, shell.pwd());
+            cloneDir(input, shell.pwd().stdout);
             shell.exit(1);
         } else console.error('ERROR: Incorrect option usage! Correct: "fot clone {template name(string)} {folder name upon creation(string)}"');
         break;
